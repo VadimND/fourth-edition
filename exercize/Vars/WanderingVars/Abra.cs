34a7e51993193e096ee5893e27d2174e7aa6d018
VadimND <webzorka24@gmail.com>
2023-01-19 17:01:51 +0300

    Wandering Vars

---
+++ exercize/Vars/WanderingVars/Abra.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WanderingVars
+{
+    internal class Abra
+    {
+        public int Vavavoom;
+
+        public bool Lala(int floq)
+        {
+            if (floq < Vavavoom)
+            {
+                Vavavoom += floq;
+                return true;
+            }
+            return false;
+        }
+    }
+}
