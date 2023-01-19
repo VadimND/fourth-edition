beb1ca28c4049bf3c033c87d7f223c829fd1f5c5
VadimND <webzorka24@gmail.com>
2023-01-19 17:01:51 +0300

    Wandering Vars

---
+++ exercize/Vars/WanderingVars/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WanderingVars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {          
+            Pizza foxtrot = new Pizza() { Zippo = 2 };
+            foxtrot.Bamboo(foxtrot.Zippo);
+            Pizza november = new Pizza() { Zippo = 3 };
+            Abra tango = new Abra() { Vavavoom = 4 };
+            int count = 0;
+            while (tango.Lala(november.Zippo))
+            {
+                count++;
+                november.Zippo *= -1;
+                november.Bamboo(tango.Vavavoom);
+                foxtrot.Bamboo(november.Zippo);
+                tango.Vavavoom -= foxtrot.Zippo;
+            }
+            Console.WriteLine("november.Zippo = " + november.Zippo);
+            //Console.WriteLine("foxtrot.Zippo = " + foxtrot.Zippo);
+            //Console.WriteLine("tango.Vavavoom = " + tango.Vavavoom);
+            //Console.WriteLine("count = " + count);
+            Console.ReadLine();
+        }
+    }
+}
