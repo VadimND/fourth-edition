b2261f566f4535d8a514b5a09f0924cfb97534ad
VadimND <webzorka24@gmail.com>
2023-01-25 17:01:22 +0300

    Add Elephant

---
+++ exercize/Elephant/ElephantApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElephantApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Elephant lucinda = new Elephant() { Name = "Lucinda", EarSize = 33 };
+            Elephant lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+
+            Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+
+            while(true)
+            {
+                char keyChar = Console.ReadKey(true).KeyChar;
+                Console.WriteLine("You pressed " + keyChar);
+                if (keyChar == '1')
+                {
+                    Console.WriteLine("Callind lloyd.WhoAmI()");
+                    lloyd.WhoAmI();
+                }
+                else if (keyChar == '2')
+                {
+                    Console.WriteLine("Callind lucinda.WhoAmI()");
+                    lucinda.WhoAmI();
+                }
+                else if (keyChar == '3')
+                {
+                    Elephant holder;
+                    holder = lloyd;
+                    lloyd = lucinda;
+                    lucinda = holder;
+                    Console.WriteLine("References have been swapped.");
+                }
+                else if (keyChar == '4')
+                {
+                    lloyd = lucinda;
+                    lloyd.EarSize = 4321;
+                    lloyd.WhoAmI();
+                }
+                else if(keyChar == '5')
+                {
+                    lucinda.SpeakTo(lloyd, "Hi, Lloyd!");
+                }
+                else return;               
+            }          
+            
+        }
+        
+    }
+}
