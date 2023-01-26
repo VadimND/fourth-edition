2723162ae28c748e2d18d69c1ea5ea0bf2c95864
VadimND <webzorka24@gmail.com>
2023-01-26 13:31:15 +0300

    Created and tested project with menu

---
+++ exercize/Random/RandomApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomInt = random.Next();
+            Console.WriteLine(randomInt);
+
+            int zerotoNine = random.Next();
+            Console.WriteLine(zerotoNine);
+
+            int dieRoll = random.Next(1, 7);
+            Console.WriteLine(dieRoll);
+
+            double randomDouble = random.NextDouble();
+            Console.WriteLine(randomDouble * 100);
+
+            Console.WriteLine((float)randomDouble * 100F);
+            Console.WriteLine((decimal)randomDouble * 100M);
+
+            int zeroOrone = random.Next(2);
+            bool coinFlip = Convert.ToBoolean(zeroOrone);
+            Console.WriteLine(coinFlip);
+            Console.ReadLine();
+        }
+    }
+}
