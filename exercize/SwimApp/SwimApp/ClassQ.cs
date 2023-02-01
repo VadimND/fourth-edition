a2f7e3b93f9028e196c4ad85286df5b8eab0c791
VadimND <webzorka24@gmail.com>
2023-02-01 16:29:01 +0300

    Add ClassQ

---
+++ exercize/SwimApp/SwimApp/ClassQ.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwimApp
+{
+    internal class ClassQ
+    {
+        public ClassQ(bool add)
+        {
+            if (add) Op = "+";
+            else Op = "*";
+            N1 = R.Next(1, 10);
+            N2 = R.Next(1, 10);            
+        }
+        public static Random R = new Random();
+        public int N1 { get; private set; }
+        public string Op { get; private set; }
+        public int N2 { get; private set; }
+              
+        public bool Check(int a)
+        {
+            if (Op == "+") return (a == N1 + N2);
+            else return (a == N1 * N2);
+        }
+    }
+}
---
+++ exercize/SwimApp/SwimApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwimApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassQ q = new ClassQ(ClassQ.R.Next(2) == 1);
+            while (true)
+            {
+                Console.Write($"{q.N1} {q.Op} {q.N2} = ");
+                if (!int.TryParse(Console.ReadLine(), out int i))
+                {
+                    Console.WriteLine("Thanks for playing!");
+                    return;
+                }
+                if (q.Check(i))
+                {
+                    Console.WriteLine("Right!");
+                    q = new ClassQ(ClassQ.R.Next(2) == 1);
+                }
+                else Console.WriteLine("Wrong! Try again.");
+            }
+        }
+        
+    }
+}
