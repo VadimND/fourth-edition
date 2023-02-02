6737db714601d9ecfb92cc63faa3f5eaa49f89e5
VadimND <webzorka24@gmail.com>
2023-02-02 16:03:59 +0300

    Add project

---
+++ exercize/SwimApp/SwimApp/ClassCoins.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwimApp
+{
+    internal class ClassCoins
+    {
+        private int gumballs;
+        private int price;
+        public int Price { get { return price; } }
+
+        public ClassCoins(int gumballs, int price)
+        {
+            gumballs = this.gumballs;
+            price = Price;
+        }
+
+        private string DispenseOneGumballs(int price, int coinsInserted)
+        {   
+            if(this.coinsInserted >= price)
+            {
+                gumballs -= 1;
+                return "Here's your gumballs";
+            } else 
+            {
+                return "Insert more coins";
+            }
+        }
+    }
+}
---
+++ exercize/gumble/GumbleMachine/ClassCoins.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwimApp
+{
+    internal class ClassCoins
+    {
+        private int gumballs;
+        private int price;
+        public int Price { get { return price; } }
+
+        public ClassCoins(int gumballs, int price)
+        {
+            gumballs = this.gumballs;
+            price = Price;
+        }
+
+        private string DispenseOneGumballs(int price, int coinsInserted)
+        {   
+            // Проверка резервного поля price
+            if(this.coinsInserted >= price)
+            {
+                gumballs -= 1;
+                return "Here's your gumballs";
+            } else 
+            {
+                return "Insert more coins";
+            }
+        }
+    }
+}
