1db77483d84f2cb71e7441a7d7dd8979acf16d2e
VadimND <webzorka24@gmail.com>
2023-02-03 10:49:57 +0300

    Improve encapsulation

--- exercize/Sword/SwordApp/Program.cs
+++ exercize/Sword/SwordApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,21 +9,31 @@ namespace SwordApp
 {
     internal class Program
     {
+        static Random random = new Random();
         static void Main(string[] args)
-        {
-            Random random = new Random();
-            SwordDamage swordDamage = new SwordDamage();
+        {            
+            SwordDamageNext swordDamage = new SwordDamageNext(RollDice());
             while (true)
             {
                 Console.Write("0 for magic/flaming, 1 for magic, 2 for flaming, " + "3 for both, anything else to quit: ");
                 char key = Console.ReadKey().KeyChar;
                 if (key != '0' && key != '1' && key != '2' && key != '3') return;
+                swordDamage.Roll = RollDice();
+                swordDamage.Magic = (key == '1' || key == '3');
+                swordDamage.Flaming = (key == '2' || key == '3');
+                Console.WriteLine($"\nRolled {swordDamage.Roll } for {swordDamage.Damage} HP\n");
+                /*
                 int roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
                 swordDamage.Roll = roll;
                 swordDamage.SetMagic(key == '1' || key == '3');
                 swordDamage.SetFlaming(key == '2' || key == '3');
                 Console.WriteLine("\nRolled " + roll + " for " + swordDamage.Damage + " HP\n");
-            }
+                */
+            }           
         }
+        private static int RollDice()
+        {
+            return random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+        }        
     }
 }
---
+++ exercize/Sword/SwordApp/SwordDamageNext.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwordApp
+{
+    internal class SwordDamageNext
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+
+        public int Damage { get; private set; }
+        private int roll;
+        public int Roll { get { return roll; } set { roll = value;  CalculateDamage(); } }
+
+        private bool magic;
+        public bool Magic { get { return magic; } set { magic = value;  CalculateDamage(); } }
+
+        private bool flaming;     
+        public bool Flaming { get { return flaming; } set { flaming = value;  CalculateDamage(); } }
+
+        private void CalculateDamage()
+        {
+            decimal MagicMultiplier = 1M;
+            if (Magic) MagicMultiplier = 1.75M;
+
+            Damage = BASE_DAMAGE;
+            Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE;
+
+            if (Flaming) Damage += FLAME_DAMAGE;
+        }
+
+        public SwordDamageNext(int startingRoll) 
+        { 
+            roll = startingRoll;
+            CalculateDamage();
+        }
+       
+        // public decimal MagicMultiplier = 1M;
+        // public int FlamingDamage = 0;
+        // public int Damage;
+
+        
+        /*
+        public void SetMagic(bool isMagic)
+        {
+            if (isMagic)
+            {
+                MagicMultiplier = 1.75M;
+            }
+            else
+            {
+                MagicMultiplier = 1M;
+            }
+            CalculateDamage();
+        }
+        
+        public void SetFlaming(bool isFlaming)
+        {
+            CalculateDamage();
+            if (isFlaming)
+            {
+                Damage += FLAME_DAMAGE;
+            }
+        }
+        */
+    }
+}
