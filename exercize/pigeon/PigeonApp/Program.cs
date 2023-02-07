66229b632e88e5f4a0b5b55c3b3b7c5ae8bde349
VadimND <webzorka24@gmail.com>
2023-02-07 10:52:37 +0300

    Add Pigeon Project

---
+++ exercize/pigeon/PigeonApp/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigeonApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Bird bird;
+                Console.Write("\nPress P for pigeon, O for ostrich: ");
+                char key = Char.ToUpper(Console.ReadKey().KeyChar);
+                if (key == 'P') bird = new Pigeon();
+                else if (key == 'O') bird = new Ostrich();
+                else return;
+                Console.Write("\nHow many eggs should it lay? ");
+                if (!int.TryParse(Console.ReadLine(), out int numberOfEggs)) return;
+                Egg[] eggs = bird.LayEggs(numberOfEggs);
+                foreach (Egg egg in eggs)
+                {
+                    Console.WriteLine(egg.Description);
+                }
+            }
+        }
+    }
+    class Egg
+    {
+        public double Size { get; private set; }
+        public string Color { get; private set; }
+        public Egg(double size, string color)
+        {
+            Size = size;
+            Color = color;
+        }
+        public string Description
+        {
+            get { return $"A {Size:0.0}cm {Color} egg"; }
+        }
+    }
+    abstract class Bird
+    {
+        public static Random Randomizer = new Random();
+        public abstract Egg[] LayEggs(int numberOfEggs);
+    }
+    class Pigeon : Bird
+    {
+        public override Egg[] LayEggs(int numberOfEggs)
+        {
+            Egg[] eggs = new Egg[numberOfEggs];
+            for (int i = 0; i < numberOfEggs; i++)
+            {
+                eggs[i] = new Egg(Bird.Randomizer.NextDouble() * 2 + 1, "white");                
+            }
+            return eggs;            
+        }
+    }
+    class Ostrich : Bird
+    {
+        public override Egg[] LayEggs(int numberOfEggs)
+        {
+            Egg[] eggs = new Egg[numberOfEggs];
+            for (int i = 0; i < numberOfEggs; i++)
+            {
+                eggs[i] = new Egg(Bird.Randomizer.NextDouble() + 12, "speckled");
+            }
+            return eggs;
+        }
+    }
+}
