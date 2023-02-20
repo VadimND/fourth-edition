842b016928c55087227392ce3788488a91d71aed
VadimND <webzorka24@gmail.com>
2023-02-20 16:57:58 +0300

    Save AnimalZooApp

--- exercize/AnimalZooApp/Program.cs
+++ exercize/AnimalZooApp/Program.cs
@@ -6,6 +6,43 @@ using System.Threading.Tasks;
 
 namespace AnimalZooApp
 {
+    abstract class Animal
+    {
+        public abstract void MakeNoise();
+    }
+    class Hippo : Animal
+    {
+        public override void MakeNoise ()
+        {
+            Console.WriteLine("Grunt.");
+        }
+        public void Swim()
+        {
+            Console.WriteLine("Splash! I'm going for a swim!");
+        }
+    }
+    abstract class Canine : Animal
+    {
+        public bool BelongsToPack { get; protected set; } = false;
+    }
+    class Wolf : Canine
+    {
+        public Wolf(bool belongsToPack)
+        {
+            BelongsToPack = belongsToPack;
+        }
+        public override void MakeNoise()
+        {
+            if (BelongsToPack) Console.WriteLine("I'm in a pack.");
+            Console.WriteLine("Arooooo!");
+        }
+        public void HuntInPack()
+        {
+            if (BelongsToPack) Console.WriteLine("I'm going thunting with my pack!");
+            else Console.WriteLine("I'm not in a pack.");
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
