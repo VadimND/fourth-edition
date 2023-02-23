8ecddf4db5bffcd427831db1304848e841d98922
VadimND <webzorka24@gmail.com>
2023-02-23 14:23:53 +0300

    Add CardApp

---
+++ exercize/CardApp/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardApp
+{
+    internal class Card
+    {
+        public Values Value { get; private set; }
+        public Suits Suit { get; private set; }
+
+        public Card(Values value, Suits suit)
+        {
+            this.Suit = suit;
+            this.Value = value;
+        }
+        public string Name
+        {
+            get { return $"{Value} of {Suit}"; }
+        }
+    }
+}
---
+++ exercize/CardApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CardApp
+{
+    internal class Program
+    {
+        private static readonly Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            Card card = new Card((Values)random.Next(1, 14), (Suits)random.Next(4));
+            Console.WriteLine(card.Name);
+        }
+    }
+}
