4de388b990ae56385f0a80c5e8f9ada52a8043f8
VadimND <webzorka24@gmail.com>
2023-02-24 11:45:13 +0300

    Did exercises

---
+++ exercize/DuckApp/Program.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+
+namespace DuckApp
+{
+    class Duck : IComparable<Duck>
+    {
+        public int Size { get; set; }
+        public KindOfDuck Kind { get; set; }
+
+        public int CompareTo(Duck duckToCompare)
+        {
+            if (this.Size > duckToCompare.Size)
+            {
+                return 1;
+            }
+            else if (this.Size < duckToCompare.Size)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }        
+    }
+    enum KindOfDuck
+    {
+        Mallard,
+        Muscovy,
+        Loon,
+    }
+    class DuckComparerBySize : IComparer<Duck>
+    {
+        public int Compare(Duck x, Duck y)
+        {
+            if(x.Size < y.Size)
+            {
+                return -1;
+            }
+            if (x.Size > y.Size)
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+    class DuckComparerByKind : IComparer<Duck>
+    {
+        public int Compare(Duck x, Duck y)
+        {
+            if (x.Kind < y.Kind)
+            {
+                return -1;
+            }
+            if (x.Kind > y.Kind)
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Duck> ducks = new List<Duck>
+            {
+                new Duck() {Kind = KindOfDuck.Mallard, Size = 17 },
+                new Duck() {Kind = KindOfDuck.Muscovy, Size = 18 },
+                new Duck() {Kind = KindOfDuck.Loon, Size = 15 },
+                new Duck() {Kind = KindOfDuck.Muscovy, Size = 11 },
+                new Duck() {Kind = KindOfDuck.Mallard, Size = 16 },
+                new Duck() {Kind = KindOfDuck.Loon, Size = 19 },
+                new Duck() {Kind = KindOfDuck.Mallard, Size = 13 },
+            };
+            IComparer<Duck> sizeComparer = new DuckComparerBySize();
+            IComparer<Duck> kindComparer = new DuckComparerByKind();
+            ducks.Sort(kindComparer);
+
+            //ducks.Sort(sizeComparer);
+            //ducks.Sort();
+            PrintDucks(ducks);
+        }
+        public static void PrintDucks(List<Duck> ducks)
+        {
+            foreach (Duck duck in ducks)
+            {
+                Console.WriteLine($"{duck.Size} inch {duck.Kind}");
+            }
+        }
+    }
+}
---
+++ exercize/MagnetApp/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace MagnetApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string zilch = "zero";
+            string first = "one";
+            string second = "two";
+            string third = "three";
+            string fourth = "4.2";
+            string twopointtwo = "2.2";
+
+            List<string> a = new List<string>();
+
+            a.Add(zilch);
+            a.Add(first);
+            a.Add(second);
+            a.Add(third);
+            a.RemoveAt(2);            
+            
+            if(a.Contains("two"))
+            {
+                a.Add(twopointtwo);
+            }
+            if (a.Contains("three"))
+            {
+                a.Add("four");
+            }
+            if (a.IndexOf("four") != 4)
+            {
+                a.Add(fourth);
+            }
+            
+            PppPppL(a);
+            
+            Console.ReadKey();
+        }
+        static void PppPppL(List<string> a)
+        {
+            foreach (string element in a)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
