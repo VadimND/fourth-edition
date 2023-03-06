fae7dfdc4a3ae67942ccf6774999afa78b7e3339
VadimND <webzorka24@gmail.com>
2023-03-06 10:57:21 +0300

    GroupBy example add

---
+++ exercize/CardGroupApp/Suits.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardGroupApp
+{
+    enum Suits
+    {
+        Diamonds,
+        Clubs,
+        Hearts,
+        Spades,
+    }
+
+}
