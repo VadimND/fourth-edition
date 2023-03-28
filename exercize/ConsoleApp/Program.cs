130cdc8ef16bdba9b4b7fffedd463ec79847d928
VadimND <webzorka24@gmail.com>
2023-03-28 12:31:41 +0300

    Task with Streamwriter

---
+++ exercize/ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static IEnumerable<string> SimpleEnumerable()
+        {
+            yield return "apples";
+            yield return "oranges";
+            yield return "bananas";
+            yield return "unicorns";
+        }
+        static void Main(string[] args)
+        {
+            foreach (var s in SimpleEnumerable()) Console.WriteLine(s);
+        }
+    }
+}
